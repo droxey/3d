30c1a656b39e52b32b8552d352130a42da671581
Dani Roxberry <droxey@gmail.com>
2019-01-05 20:52:44 -0800

    [add] comments, error handling, header

--- reinstall.sh
+++ reinstall.sh
@@ -1,10 +1,30 @@
+#!/bin/bash
+# 
+# Sync a standard Klipper install with the lastest commit in master.
+
+# Navigate to standard Klipper install.
 cd ~/klipper
-git pull
+
+# Pull the latest code on master.
+git pull origin master
+
+# Update Pi installation as prescribed in the Klipper documentation.
 ~/klipper/scripts/install-octopi.sh
 
+# Compile any updates.
 make clean
 make
 
+# Stop Klipper service.
 sudo service klipper stop
+
+# Flash the new firmware that we compiled earlier.
 make flash FLASH_DEVICE=/dev/ttyUSB0
+
+# Start the Klipper service with new firmware.
 sudo service klipper start
+
+# Error handling.
+# Exit immediately if a command fails.
+set -o nounset
+set -o errexit
